1e115721f78aea144d893c9b8e614adf78326bec
kilianruiz <451093.joan23@fje.edu>
2025-02-12 18:49:19 +0100

    Modelos creados con las relaciones

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,14 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);  //un user pertenece a un rol
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Valoracion::class); //un user tiene muchas valoraciones
+    }
 }
