e8f96c13528876841d06da38bf31519b7ca70268
kilianruiz <451093.joan23@fje.edu>
2025-02-12 09:44:05 +0100

    Migraciones hechas

---
+++ database/migrations/2025_02_11_195028_create_fotos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fotos', function (Blueprint $table) {
+            $table->id();
+            $table->string('path'); //ruta de la img
+            // Clave foránea hacia la tabla 'restaurants'
+            $table->unsignedBigInteger('restaurant_id');
+            $table->foreign('restaurant_id')->references('id')->on('restaurantes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fotos');
+    }
+};
