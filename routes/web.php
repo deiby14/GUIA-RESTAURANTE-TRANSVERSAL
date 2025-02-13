d60c1d8a580eb9e732309d8d919e0d1e0f41251d
kilianruiz <451093.joan23@fje.edu>
2025-02-13 21:47:26 +0100

    Nuevas rutas e imagenes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RestauranteController;
+use App\Http\Controllers\FavoriteController;
 
 // Ruta para la página de bienvenida
 Route::get('/', function () {
@@ -18,3 +21,10 @@ Route::post('login', [LoginController::class, 'login']);
 // Ruta para cerrar sesión
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');  
 
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+// Ruta para la lista de restaurantes
+Route::get('/restaurantes', [RestauranteController::class, 'index'])->name('restaurantes.index');
+
+// Ruta para la lista de favoritos
+Route::get('/favorites', [FavoriteController::class, 'index'])->name('favorites.index');
