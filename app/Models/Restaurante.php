88cf758a0e3f6567fa0a23d75cb03419ec3808cf
kilianruiz <451093.joan23@fje.edu>
2025-02-17 18:19:52 +0100

    Modelos de las nuevas tablas

--- app/Models/Restaurante.php
+++ app/Models/Restaurante.php
@@ -7,13 +7,33 @@ use Illuminate\Database\Eloquent\Model;
 class Restaurante extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'direccion',
+        'precio_medio',
+        'tipocomida_id',
+        'ciudad_id',
+    ];
+    
     public function fotos()
     {
-        return $this->hasMany(Foto::class);    //un restaurante tiene muchas fotos
+        return $this->hasMany(Foto::class);  
     }
 
     public function valoraciones()
     {
-        return $this->hasMany(Valoracion::class);  //un restaurante tiene muchas valoraciones
+        return $this->hasMany(Valoracion::class);  
     }
+
+    public function tipoComida()
+        {
+            return $this->belongsTo(Tipocomida::class);
+        }
+    
+    public function ciudad()
+        {
+            return $this->belongsTo(Ciudad::class);
+        }
 }
